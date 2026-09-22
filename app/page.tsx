445056bebc4ff995ab233123e0bdcc8c86664e55
AdhamAzri <adhamazri86@gmail.com>
2026-09-23 00:53:28 +0800

    [Fix] delete close square bracket

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                   </section>
 
                   <section id="mempelai">
-                      <SangMempelaiPageComponent/>]
+                      <SangMempelaiPageComponent/>
                   </section>
 
               </div>
